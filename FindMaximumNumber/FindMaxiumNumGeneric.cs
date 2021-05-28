834dc68d6c9fd39b8729aefc86403384a274dc69
shivam satpute <shivam897satpute@gmail.com>
2021-05-28 23:27:45 +0530

    [shivam]add: Refactor1 and Refactor2

---
+++ FindMaximumNumber/FindMaxiumNumGeneric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class FindMaxiumNumGeneric<R> where R : IComparable 
+    {
+        public R Take_input1, Take_input2, Take_input3; 
+        public FindMaxiumNumGeneric(R Take_input1, R Take_input2, R Take_input3)
+        {
+            this.Take_input1 = Take_input1;
+            this.Take_input2 = Take_input2;
+            this.Take_input3 = Take_input3;
+        }
+        public R getMaximum()
+        {
+            var MaxResult = this.Take_input1.CompareTo(Take_input2) > 0 ? Take_input1 : (this.Take_input2.CompareTo(Take_input3) > 0 ? Take_input2 : Take_input3); 
+            Console.WriteLine("Maximum Number is  :" + MaxResult);
+            return MaxResult;
+        }
+    }
+}
--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -10,10 +10,18 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            FindMaximumNum FindMax = new FindMaximumNum();
-            FindMax.FindMaxInteger(10, 20, 150);
-            FindMax.FindMaxFloat(50.4, 890.2, 159.89);
-            FindMax.FindMaxString("Apple", "Peach", "Banana");
+            //FindMaximumNum FindMax = new FindMaximumNum();
+            //FindMax.FindMaxInteger(10, 20, 150);
+            //FindMax.FindMaxFloat(50.4, 890.2, 159.89);
+            //FindMax.FindMaxString("Apple", "Peach", "Banana");
+            FindMaxiumNumGeneric<int> intMax = new FindMaxiumNumGeneric<int>(55, 599, 5100);
+            intMax.getMaximum();
+
+            FindMaxiumNumGeneric<double> doubleMax = new FindMaxiumNumGeneric<double>(3.3, 55.25, 10.5); 
+            doubleMax.getMaximum();
+
+            FindMaxiumNumGeneric<string> stringMax = new FindMaxiumNumGeneric<string>("Apple", "Banana", "Peach"); 
+            stringMax.getMaximum();
             Console.ReadLine();
 
         }
